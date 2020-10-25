f21c6cd692bdae3293056e3ad0f3bb1683d08c47
Ashish Papanai <ashishpapanai00@gmail.com>
2020-10-25 09:11:15 +0530

    Day 3: FEN notation added

--- Js/board.js
+++ Js/board.js
@@ -1,11 +1,181 @@
 let GameBoard = {};
 
 GameBoard.pieces = new Array(BRD_SQ_NUM);
-GameBoard.side = COLOUR.WHITE;
+GameBoard.side = COLOURS.WHITE;
 GameBoard.fityMove = 0;
 GameBoard.hisPly = 0; // will be used as index of array storing all moves in the game.
 GameBoard.ply = 0; // number of half moves made in the search tree.
+GameBoard.enPas = 0; //for en-Passent
 GameBoard.castlePerm = 0; //will keep a track of castling permission. 
 GameBoard.material = new Array(2); //WHITE, BLACK material of pieces
 GameBoard.pceNum = new Array(13); //Number of unique Pieces in game
-GameBoard.pList = new Array(14*10);
+GameBoard.pList = new Array(14 * 10);
+GameBoard.posKey = 0;
+
+GameBoard.moveList = new Array(MAXDEPTH * MAXPOSITIONMOVES);
+GameBoard.moveScores = new Array(MAXDEPTH * MAXPOSITIONMOVES);
+GameBoard.moveListStart = new Array(MAXDEPTH);
+
+function GeneratePosKey() {
+    let sq = 0;
+    let finalKey = 0;
+    let piece = PIECES.EMPTY;
+    for (sq = 0; sq < BRD_SQ_NUM; ++sq) {
+        piece = GameBoard.pieces[sq];
+        if (piece != PIECES.EMPTY && piece != SQUARES.OFFBOARD) {
+            finalKey ^= PieceKeys[(piece * 120) + sq];
+        }
+    }
+    if (GameBoard.side == COLOURS.WHITE) {
+        finalKey ^= SideKey;
+    }
+    if (GameBoard.enPas != SQUARES.NO_SQ) {
+        finalKey ^= PieceKeys[GameBoard.enPas];
+    }
+    finalKey ^= CastleKeys[GameBoard.castlePerm];
+}
+
+function ResetBoard() {
+    let index = 0;
+    for (index = 0; index < BRD_SQ_NUM; ++index) {
+        GameBoard.pieces[index] = SQUARES.OFFBOARD;
+    }
+    for (index = 0; index < 64; ++index) {
+        GameBoard.pieces[SQ120(index)] = PIECES.EMPTY;
+    }
+    for (index = 0; index < 14 * 120; ++index) {
+        GameBoard.pList[index] = PIECES.EMPTY;
+    }
+    for (index = 0; index < 2; ++index) {
+        GameBoard.material[index] = 0;
+    }
+    for (index = 0; index < 13; ++index) {
+        GameBoard.pceNum[index] = 0;
+    }
+    GameBoard.side = COLOURS.BOTH;
+    GameBoard.enPas = SQUARES.NO_SQ;
+    GameBoard.fiftyMove = 0;
+    GameBoard.ply = 0;
+    GameBoard.hisPly = 0;
+    GameBoard.CastlePerm = 0;
+    GameBoard.posKey = 0;
+    GameBoard.moveListStart[GameBoard.ply] = 0;
+}
+
+function ParseFen(fen) {
+    ResetBoard();
+    let rank = RANKS.RANK_0;
+    let file = FILES.FILE_A;
+    let piece = 0;
+    let count = 0;
+    let i = 0;
+    let sq120 = 0;
+    let fenCnt = 0;
+    while ((rank >= RANKS.RANK_1) && fenCnt < fen.length) {
+        count = 1;
+        switch (fen[fenCnt]) {
+            case 'p':
+                piece = PIECES.bP;
+                break;
+            case 'r':
+                piece = PIECES.bR;
+                break;
+            case 'n':
+                piece = PIECES.bN;
+                break;
+            case 'b':
+                piece = PIECES.bB;
+                break;
+            case 'k':
+                piece = PIECES.bK;
+                break;
+            case 'q':
+                piece = PIECES.bQ;
+                break;
+            case 'P':
+                piece = PIECES.wP;
+                break;
+            case 'R':
+                piece = PIECES.wR;
+                break;
+            case 'N':
+                piece = PIECES.wN;
+                break;
+            case 'B':
+                piece = PIECES.wB;
+                break;
+            case 'K':
+                piece = PIECES.wK;
+                break;
+            case 'Q':
+                piece = PIECES.wQ;
+                break;
+
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+                piece = PIECES.EMPTY;
+                count = fen[fenCnt].charCodeAt() - '0'.charCodeAt();
+                break;
+
+            case '/':
+            case ' ':
+                rank--;
+                file = FILES.FILE_A;
+                fenCnt++;
+                continue;
+            default:
+                console.log("FEN error");
+                return;
+
+        }
+
+        for (i = 0; i < count; i++) {
+            sq120 = FR2SQ(file, rank);
+            GameBoard.pieces[sq120] = piece;
+            file++;
+        }
+        fenCnt++;
+    }
+    GameBoard.side = (fen[fenCnt] == 'w') ? COLOURS.WHITE : COLOURS.BLACK;
+    fenCnt += 2;
+
+    for (i = 0; i < 4; i++) {
+        if (fen[fenCnt] == ' ') {
+            break;
+        }
+        switch (fen[fenCnt]) {
+            case 'K':
+                GameBoard.castlePerm |= CASTLEBIT.WKCA;
+                break;
+            case 'Q':
+                GameBoard.castlePerm |= CASTLEBIT.WQCA;
+                break;
+            case 'k':
+                GameBoard.castlePerm |= CASTLEBIT.BKCA;
+                break;
+            case 'q':
+                GameBoard.castlePerm |= CASTLEBIT.BQCA;
+                break;
+            default:
+                break;
+        }
+        fenCnt++;
+    }
+    fenCnt++;
+
+    if (fen[fenCnt] != '-') {
+        file = fen[fenCnt].charCodeAt() - 'a'.charCodeAt();
+        rank = fen[fenCnt + 1].charCodeAt() - '1'.charCodeAt();
+        console.log("fen[fenCnt]:" + fen[fenCnt] + " File:" + file + " Rank:" + rank);
+        GameBoard.enPas = FR2SQ(file, rank);
+    }
+
+    GameBoard.posKey = GeneratePosKey();
+
+}
--- Js/main.js
+++ Js/main.js
@@ -1,32 +1,66 @@
-$(function(){
+$(function() {
     init();
     console.log("Main Init Called");
+
 });
 
-function InitFilesRanksBrd(){
+function InitFilesRanksBrd() {
     let index = 0;
     let file = FILES.FILE_A;
-    var rank = RANKS.RANK_1;
-    var sq = SQUARES.A1;
+    let rank = RANKS.RANK_1;
+    let sq = SQUARES.A1;
 
-    for(index = 0; index < BRD_SQ_NUM; ++index){
+    for (index = 0; index < BRD_SQ_NUM; ++index) {
         FilesBrd[index] = SQUARES.OFFBOARD;
         RanksBrd[index] = SQUARES.OFFBOARD;
     }
 
-    for(rank = RANKS.RANK_1; rank <= RANKS.RANK_8; ++rank){
-        for(file = FILES.FILE_A; file <= FILES.FILE_H; ++file){
+    for (rank = RANKS.RANK_1; rank <= RANKS.RANK_8; ++rank) {
+        for (file = FILES.FILE_A; file <= FILES.FILE_H; ++file) {
             sq = FR2SQ(file, rank);
             FilesBrd[sq] = file;
             RanksBrd[sq] = rank;
         }
     }
-    console.log("FilesBrd[0]: "+FilesBrd[0]+" RankBrd[0]: "+RanksBrd[0]);
-    console.log("FilesBrd[SQUARES.A1]: "+FilesBrd[SQUARES.A1]+" RankBrd[SQUARES.A1]: "+RanksBrd[SQUARES.A1]);
-    console.log("FilesBrd[SQUARES.E8]: "+FilesBrd[SQUARES.E8]+" RankBrd[SQUARES.E8]: "+RanksBrd[SQUARES.E8]);
+    console.log("FilesBrd[0]: " + FilesBrd[0] + " RankBrd[0]: " + RanksBrd[0]);
+    console.log("FilesBrd[SQUARES.A1]: " + FilesBrd[SQUARES.A1] + " RankBrd[SQUARES.A1]: " + RanksBrd[SQUARES.A1]);
+    console.log("FilesBrd[SQUARES.E8]: " + FilesBrd[SQUARES.E8] + " RankBrd[SQUARES.E8]: " + RanksBrd[SQUARES.E8]);
 }
 
-function init(){
+function InitHashKeys() {
+    let index = 0;
+    for (index = 0; index < 14 * 120; ++index) {
+        pieceKeys[index] = RAND_32();
+    }
+    SideKey = RAND_32();
+    for (index = 0; index < 16; ++index) {
+        CastleKeys[index] = RAND_32()
+    }
+}
+
+function InitSq120To64() {
+    let index = 0;
+    let file = FILES.FILE_A;
+    let rank = RANKS.RANK_A;
+    let sq = SQUARES.A1;
+    let sq64 = 0;
+    for (index = 0; index < BRD_SQ_NUM; ++index) {
+        Sq120ToSq64[index] = 65;
+    }
+    for (index = 0; index < 64; ++index) {
+        Sq64ToSq120[index] = 120;
+    }
+    for (rank = RANKS.RANK_1; rank <= RANKS.RANK_8; ++rank) {
+        for (file = FILES.FILE_A; file <= FILES.FILE_H; ++file) {
+            sq = FR2SQ(file, rank);
+            Sq64ToSq120[sq64] = sq;
+            Sq120ToSq64[sq] = sq64;
+            sq64++;
+        }
+    }
+}
+
+function init() {
     console.log("init() called");
     InitFilesRanksBrd();
 }
